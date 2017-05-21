a34e97a9ccf10eb6e4d018066c0e5898c0e07b99
K. Sabbak <email": null>
2017-05-21 11:59:35 -0500

    Write things to create and check auth tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  include Response
+  include ExceptionHandler
   # TODO: Temporarily commenting this out in order to POST to Rails API without needing CSRF authenticity token. Not secure, but fine for now.
   # protect_from_forgery with: :exception
+
+  before_action :authorize_request
+  attr_reader :current_user
+
+
+
+  private
+  def authorize_request
+    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,21 @@
 class UsersController < ApplicationController
+  skip_before_action :authorize_request, only: [:create, :new]
+
   def create
     user = User.from_oauth(user_params)
     session[:user_id] = user.id
-    render json: user
+    auth_token = JsonWebToken.encode(user_id: user.id)
+    p auth_token
+    json_response(auth_token: auth_token)
   end
 
   def update
   end
 
   def show
+  end
+
+  def new
   end
 
   private
