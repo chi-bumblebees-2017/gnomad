e60be7998838413b62bd4e8601031f2ffb2916f2
Simon Swartzman <email": null>
2017-05-21 09:49:07 -0500

    Make initial scopes, add comments for other scopes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,12 @@ class User < ApplicationRecord
   has_one :gnomad_profile
   has_one :localhost_profile
   scope :from_location, ->(city, state) { where(home_city: city, home_state: state) }
+  delegate :available, :suggestions, to: :localhost_profile, allow_nil: true
   scope :available, -> { where(available: true) }
-  scope :as_traveler
-  scope :likes_any_while_traveling, ->(interests) { where("")}
-  scope :likes_any_while_hosting, ->(interests) { joins(:localhost_profile).where("restaurants = :restaurants OR sports = :sports OR museums = :museums OR bars = :bars OR music = :music OR outdoors = :outdoors OR art = :art OR fitness = :fitness OR history = :history OR architecture = :architecture OR family_fun = :family_fun OR zoo = :zoo OR culture = :culture OR volunteer = :volunteer OR shopping = :shopping", ) }
-
-
   # make these scopes work, using delegate to pass profile booleans that this user owns
-  # e.g.
+  # scope :as_traveler
+  # scope :likes_any_while_traveling, ->(interests) { where("")}
+  # scope :likes_any_while_hosting, ->(interests) { joins(:localhost_profile).where("restaurants = :restaurants OR sports = :sports OR museums = :museums OR bars = :bars OR music = :music OR outdoors = :outdoors OR art = :art OR fitness = :fitness OR history = :history OR architecture = :architecture OR family_fun = :family_fun OR zoo = :zoo OR culture = :culture OR volunteer = :volunteer OR shopping = :shopping", ) }
   def self.from_oauth(user_params)
     user = User.find_or_create_by(uid: user_params[:uid], provider: "facebook")
     user.first_name ||= user_params["first_name"]
