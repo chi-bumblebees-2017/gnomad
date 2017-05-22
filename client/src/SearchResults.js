ed8e446e2d8d48bef986bafc23f1a66964d13fe9
Stacy Curry & Tove N Mercer <pair+stacurry.tovemercer@devbootcamp.com>
2017-05-22 10:49:15 -0500

    style search results page

--- client/src/SearchResults.js
+++ client/src/SearchResults.js
@@ -10,15 +10,16 @@ class SearchResults extends Component {
   render() {
     if (this.props.results.length === 0) {
       return (
-        <div className="search-results">
-          No localhosts found in that city that share your interests...
+        <div className="search-results ui comments">
+          <p className="margin-none">No localhosts found in that city</p>
+          <p className="margin-none">that share your interests...</p>
         </div>
       );
     } else {
       return (
-        <ul className="search-results">
+        <ul className="search-results ui comments top-margin-10">
           {this.props.results.map((localhost) =>
-            <UserListItemContainer key={localhost.id} user={localhost} snippet={localhost.suggestions} linkto={`/users/${localhost.first_name}/${localhost.id}`} />
+            <UserListItemContainer key={localhost.id} user={localhost} snippet={localhost.bio} linkto={`/users/${localhost.first_name}/${localhost.id}`} />
           )}
         </ul>
       );
