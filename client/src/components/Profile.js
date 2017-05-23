8c95ddc965ce757a2acd294f7c99c8ddda4cc81e
Joe LeBeau <email": null>
2017-05-23 16:53:44 -0500

    pulling down remote master

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -52,7 +52,7 @@ class Profile extends Component {
         return (
           <div className="profile-container ui centered container">
             <div className="max-width">
-              <div className='max-width'>
+              <div className='max-width '>
                 <h1 className='inline'>{this.state.userData.user.first_name}</h1>
                 <Star action={this.toggleStar} starred={this.state.starred} userID={this.state.userData.user.id}/>
               </div>
