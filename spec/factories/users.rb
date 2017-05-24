f8a0549c9c366bf0b342aa169604aa6df6795be3
K. Sabbak <email": null>
2017-05-24 17:50:38 -0500

    Write tests for updating the gnomad and localhost profiles of users. Fix commenting out of factories.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,15 @@
-# FactoryGirl.define do
-#   factory :user do
-#     provider  'facebook'
-#     uid { Faker::Number.hexadecimal(5) }
-#     first_name { Faker::Name.first_name }
-#     last_name { Faker::Name.last_name }
-#     image_url  'https://thumbs.dreamstime.com/x/garden-gnome-elf-22324178.jpg'
-#     email { Faker::Internet.email }
-#     home_city  'New York'
-#     home_state  'NY'
-#   end
-# end
+FactoryGirl.define do
+  factory :user do
+    provider  'facebook'
+    uid { Faker::Number.hexadecimal(5) }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    image_url  'https://thumbs.dreamstime.com/x/garden-gnome-elf-22324178.jpg'
+    email { Faker::Internet.email }
+    home_city  'New York'
+    home_state  'NY'
+  end
+end
 
 
 
