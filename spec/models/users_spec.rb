1699b8ace636681e6a793cd67b730e3ad6ce1473
K. Sabbak <ksabbak@gmail.com>
2017-06-06 23:27:37 -0500

    Fix typo in user model test

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
 
   describe "associations" do
     it { should have_many(:initiated_conversations).with_foreign_key 'initiator_id'  }
-    it { should have_many(:received_conversations).with_foreign_key 'received_id' }
+    it { should have_many(:received_conversations).with_foreign_key 'receiver_id' }
     # For some reason the PM one isn't working even though shoulda has methods for both dependent destroy and foreign keys, so  ¯\_(ツ)_/¯
     it { should have_many(:sent_stars) }
     it { should have_many(:received_stars) }
